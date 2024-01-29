f08d12947e71d8f0d230e5e1101dd7a96cfd51c9
AWolewicz <agnieszkawolewicz5@gmail.com>
2024-01-29 11:41:54 +0100

    src + polecenia

---
+++ src/js/01-gallery.js
@@ -0,0 +1,15 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+Utwórz galerię z możliwością kliknięcia w jej elementy i przeglądania pełnego obrazu w oknie modalnym. 
+Obejrzyj wersję demonstracyjną wideo o działaniu galerii.
+
+Wykonaj to zadanie w plikach 01-gallery.html i 01-gallery.js. Działanie strony możemy “rozbić” na kilka zadań
+
+Tworzenie i renderowanie znacznika zgodnie z tablicą danych galleryItems i dostarczonym szablonem elementu galerii.
+Implementacja oddelegowania do div.gallery i otrzymania url większego obrazu.
+Połączenie skryptu i stylów biblioteki okna modalnego basicLightbox. Użyj CDN serwisu jsdelivr i dodaj do projektu linki do zminimalizowanych (.min) plików biblioteki.
+Otworzenie okna modalnego po kliknięciu w element galerii. Aby to zrobić, zapoznaj się z dokumentacją i przykładami.
+Zmiana wartości atrybutu src elementu <img> w oknie modalnym przed otworzeniem. Użyj gotowego znacznika okna modalnego z obrazem z przykładów biblioteki basicLightbox.
